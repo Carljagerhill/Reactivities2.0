5620600fedcd151ae32eae9d1f1647d8d31a616a
Carljagerhill <Carl__96@hotmail.com>
2022-01-05 00:32:06 +0100

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -37,6 +37,12 @@ namespace API
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
 
         }
 
@@ -53,6 +59,8 @@ namespace API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
